30dfc62facc0857cab73f69a977a70e68e2e1a20
Siddharth Verma <sidverma2206@gmail.com>
2024-06-25 22:00:50 +0530

    postgresql

--- ExpenseProject/settings.py
+++ ExpenseProject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'ExpenseProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypostdb',
+        'USER': 'siddharth',
+        'PASSWORD': 'SidVerma2206',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
