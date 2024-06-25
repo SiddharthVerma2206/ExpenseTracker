56f3f0c24753ce0fb4c1be702304f292647d87f0
Siddharth Verma <sidverma2206@gmail.com>
2024-06-25 14:14:18 +0530

    fixxxxx

--- ExpenseProject/settings.py
+++ ExpenseProject/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/home/ubuntu/portfolio/projects/ExpenseTracker/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS=[os.path.join(BASE_DIR,'/home/ubuntu/portfolio/projects/ExpenseTracker/')]
-STATIC_ROOT=os.path.join(BASE_DIR,'/home/ubuntu/portfolio/projects/ExpenseTracker/')
+STATIC_ROOT=os.path.join(BASE_DIR,'/home/ubuntu/portfolio/projects/ExpenseTracker/static')
 
 
 
