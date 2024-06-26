5ffc285781f7934a881686ecd0a91c13ff0415b9
Siddharth Verma <sidverma2206@gmail.com>
2024-06-26 15:51:44 +0530

    Postgresql

--- ExpenseProject/settings.py
+++ ExpenseProject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'ExpenseProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newdb',
+        'USER': 'sidverma',
+        'PASSWORD': 'sidverma2206',
+        'HOST': 'localhost',
+        'PORT': '5452'
     }
 }
 
